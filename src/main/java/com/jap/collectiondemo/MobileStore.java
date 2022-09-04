09f0e8d63e34acef156cb97d9ff7314e3746330e
HP <mdzaid1299@gmail.com>
2022-09-05 01:21:42 +0530

    initial commit

--- src/main/java/com/jap/collectiondemo/MobileStore.java
+++ src/main/java/com/jap/collectiondemo/MobileStore.java
@@ -1,8 +1,8 @@
 package com.jap.collectiondemo;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MobileStore {
@@ -15,7 +15,8 @@ public class MobileStore {
     {
         allMobiles = new ArrayList<>();
     }
-
+    Iterator<Mobile> itr = null;
+    Mobile obj = null;
     //Write logic to read the fileName that is "mobile.csv"
     //read all the lines one by one
     //Create Mobile class object and store data from file in the respective attributes of Mobile class
@@ -24,7 +25,26 @@ public class MobileStore {
     //handle all the exceptions
     public List<Mobile> readMobileData(String fileName)
     {
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String data = bufferedReader.readLine();
 
+            while((data = bufferedReader.readLine()) != null){
+
+                String[] response = data.split(splitBy);
+                String brandname = response[0];
+                String model = response[1];
+                double cost = Double.parseDouble(response[2]);
+                String screenSize = response[3] ;
+                String batterylife = response[4];
+                String storageSpace = response[5];
+                int cameraPixel = Integer.parseInt(response[6]);
+                allMobiles.add(new Mobile(brandname,model,cost,screenSize,batterylife,storageSpace,cameraPixel));
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         return allMobiles;
     }
 
@@ -33,6 +53,13 @@ public class MobileStore {
     public List<Mobile> findPhoneByBrand(String brandName)
     {
         List<Mobile> mobilesByBrand = new ArrayList<>();
+        itr = allMobiles.iterator();
+        while (itr.hasNext()){
+            obj = itr.next();
+            if(obj.getBrandName().equals(brandName)){
+                mobilesByBrand.add(obj);
+            }
+        }
 
         return mobilesByBrand;
     }
@@ -42,7 +69,13 @@ public class MobileStore {
     public List<Mobile> findPhoneCostMoreThan$500()
     {
         List<Mobile> mobilesMoreThan500 = new ArrayList<>();
-
+        itr = allMobiles.iterator();
+        while (itr.hasNext()){
+            obj = itr.next();
+            if(obj.getCost() >= 500){
+                mobilesMoreThan500.add(obj);
+            }
+        }
         return mobilesMoreThan500;
     }
 
@@ -51,10 +84,15 @@ public class MobileStore {
     public List<Mobile> findPhonePixelMoreThan12MP()
     {
         List<Mobile> mobilesMoreThan12MP = new ArrayList<>();
-
+        itr = allMobiles.iterator();
+        while(itr.hasNext()){
+            obj = itr.next();
+            if(obj.getCameraPixels() > 12){
+                mobilesMoreThan12MP.add(obj);
+            }
+        }
         return mobilesMoreThan12MP;
     }
 
 
 }
-
